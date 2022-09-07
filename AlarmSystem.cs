e6a058cea21e76087b9925c1b152410165c61346
EugeneLobski <eugene.lobski@gmail.com>
2022-09-07 15:28:32 +0300

    Update AlarmSystem.cs

--- AlarmSystem.cs
+++ AlarmSystem.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlarmSystem : MonoBehaviour
-{
+public class AlarmSystem : MonoBehaviour {
     [SerializeField] private AudioSource _alarm;
     private Coroutine _fadeIn;
     private Coroutine _fadeOut;
 
-    public void TurnOn()
-    {
+    public void TurnOn() {
         _alarm.Play();
 
         if (_fadeOut != null)
@@ -18,40 +16,34 @@ public class AlarmSystem : MonoBehaviour
         _fadeIn = StartCoroutine(FadeInSound());
     }
 
-    public void TurnOff()
-    {
+    public void TurnOff() {
         if (_fadeIn != null)
             StopCoroutine(_fadeIn);
 
         _fadeOut = StartCoroutine(FadeOutSound());
     }
 
-    private IEnumerator FadeInSound()
-    {
+    private IEnumerator FadeInSound() {
         if (_alarm.volume == 1)
             yield break;
 
         float fadeDuration = 5f;
 
-        while (_alarm.volume != 1)
-        {
+        while (_alarm.volume != 1) {
             _alarm.volume = Mathf.MoveTowards(_alarm.volume, 1f, Time.deltaTime / fadeDuration);
             yield return null;
         }
     }
 
-    private IEnumerator FadeOutSound()
-    {
-        if (_alarm.volume == 0)
-        {
+    private IEnumerator FadeOutSound() {
+        if (_alarm.volume == 0) {
             _alarm.Stop();
             yield break;
         }
 
         float fadeDuration = 5f;
 
-        while (_alarm.volume != 0)
-        {
+        while (_alarm.volume != 0) {
             _alarm.volume = Mathf.MoveTowards(_alarm.volume, 0f, Time.deltaTime / fadeDuration);
             yield return null;
         }
