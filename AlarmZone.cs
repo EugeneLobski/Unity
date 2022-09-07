5d405746d99a7ac86f3d3e5283eea88e48c85f42
EugeneLobski <eugene.lobski@gmail.com>
2022-09-07 15:29:31 +0300

    Update AlarmZone.cs

--- AlarmZone.cs
+++ AlarmZone.cs
@@ -4,20 +4,17 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class AlarmZone : MonoBehaviour
-{
+public class AlarmZone : MonoBehaviour {
     [SerializeField] private UnityEvent _cameIn;
     [SerializeField] private UnityEvent _cameOut;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+    private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.TryGetComponent<CharacterController2D>(out CharacterController2D controller)) {
             _cameIn.Invoke();
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
+    private void OnTriggerExit2D(Collider2D collision) {
         if (collision.TryGetComponent<CharacterController2D>(out CharacterController2D controller)) {
             _cameOut.Invoke();
         }
