db1d9d901d35f67c1c561463e3970aab59ca572a
EugeneLobski <eugene.lobski@gmail.com>
2022-09-07 18:09:39 +0300

    Add files via upload

---
+++ SpawnManger.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManger : MonoBehaviour
+{
+    [SerializeField] private float _spawnDelay = 2f;
+
+    void Start() {
+        Spawner[] _spawners = gameObject.GetComponentsInChildren<Spawner>();
+        StartCoroutine(SpawnEnemy(_spawners, _spawnDelay));
+    }
+
+    private IEnumerator SpawnEnemy(Spawner[] spawners, float delay) {
+        while (true) // в задаче нет условия выхода
+        {
+            foreach (Spawner spawner in spawners) {
+                spawner.CreateEnemy();
+                yield return new WaitForSeconds(delay);
+            }
+            
+            yield return null;
+        }        
+    }
+}
